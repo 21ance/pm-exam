e575a3a52843d60460af46c6443ee0e0f0da922b
Lance Lopez <lancelopez46@gmail.com>
2024-01-12 07:09:22 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./style.css"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Source Sans"],
+				sansSemiBold: ["Source Sans SemiBold"],
+				sansBold: ["Source Sans Bold"],
+			},
+			colors: {
+				gray01: "#333333",
+				gray02: "#eeeeee",
+				gray03: "#444444",
+				gray04: "#999999",
+				blue01: "#017bc1",
+			},
+		},
+	},
+	plugins: [],
+};
